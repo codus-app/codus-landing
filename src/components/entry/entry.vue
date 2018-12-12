e567a4cd736eca3e92195cb21d4ca0de82d1d5bc
Luke Taylor <luke@deentaylor.com>
2018-12-12 10:12:58 -0500

    Add inputs to entry form

--- src/components/entry/entry.vue
+++ src/components/entry/entry.vue
@@ -3,6 +3,9 @@
   <div class="entry light">
     <h3>Start learning today.</h3>
     <h3 class="green">Let's get started.</h3>
+
+    <input type="text" placeholder="Email Address">
+    <input type="text" placeholder="Password">
   </div>
 </template>
 
