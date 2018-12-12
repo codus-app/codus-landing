d6f9ed28437612e693ad8d1916f74c9dd2d0e575
Luke Taylor <luke@deentaylor.com>
2018-12-12 10:43:05 -0500

    Add modified version of input component from codus

---
+++ src/components/input/script.js
@@ -0,0 +1,23 @@
+export default {
+  props: {
+    value: String,
+
+    title: String,
+    placeholder: String,
+    message: String,
+    status: {
+      type: String,
+      validator: value => ['neutral', 'success', 'failure', 'loading'].includes(value),
+      default: 'neutral',
+    },
+    theme: {
+      type: String,
+      validator: value => ['light', 'dark'].includes(value),
+      default: 'dark',
+    },
+  },
+
+  methods: {
+    onInput(e) { this.$emit('input', e.target.value); },
+  },
+};
