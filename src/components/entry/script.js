96c1f78a478e59c8e2c63991c4548cf7c71d3706
Luke Taylor <luke@deentaylor.com>
2018-12-18 20:00:59 -0500

    Automatically turn on email/password validation after 750ms of no typing

--- src/components/entry/script.js
+++ src/components/entry/script.js
@@ -1,5 +1,7 @@
+import { debounce } from 'debounce';
 import isEmail from 'validator/lib/isEmail';
 import isByteLength from 'validator/lib/isByteLength';
+
 import * as api from '../../api';
 import auth from '../../auth';
 
@@ -54,6 +56,10 @@ export default {
       this.lastCheckedEmail = this.email;
     },
 
+    debouncedCheckEmail: debounce(function checkEmail2() { this.checkEmail(); }, 750),
+
+    debouncedPasswordValidate: debounce(function a() { this.showPasswordValidation2 = true; }, 750),
+
     submit() {
       return new Promise((resolve) => {
         if (this.mode === 'login') {
@@ -74,9 +80,12 @@ export default {
 
   watch: {
     email() {
+      this.debouncedCheckEmail();
       this.emailStatus2 = 'neutral';
       this.accountExists = null;
       this.lastCheckedEmail = null;
     },
+
+    password() { this.debouncedPasswordValidate(); },
   },
 };
