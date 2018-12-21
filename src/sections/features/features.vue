b4d4f95b730dc6acdb62d0312cca4ce3a8955395
Luke Taylor <luke@deentaylor.com>
2018-12-21 11:06:41 -0500

    Begin features section

---
+++ src/sections/features/features.vue
@@ -0,0 +1,10 @@
+<template>
+  <section id="features">
+    <container>
+      <h2>Features</h2>
+    </container>
+  </section>
+</template>
+
+<style scoped lang="sass" src="./style.sass"></style>
+<script src="./script.js"></script>
--- src/sections/index.js
+++ src/sections/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 
 Vue.component('top-bar', require('./top-bar/top-bar.vue').default);
 Vue.component('page-header', require('./header/header.vue').default);
+Vue.component('features-section', require('./features/features.vue').default);
 
 export default [
   {
