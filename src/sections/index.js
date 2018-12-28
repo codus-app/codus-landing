0cee9183cd6d16a6b4b99476f79ea2f0bf4d2465
Luke Taylor <luke@deentaylor.com>
2018-12-28 01:49:15 -0500

    Temporarily hide features section

--- src/sections/index.js
+++ src/sections/index.js
@@ -4,9 +4,4 @@ Vue.component('top-bar', require('./top-bar/top-bar.vue').default);
 Vue.component('page-header', require('./header/header.vue').default);
 Vue.component('features-section', require('./features/features.vue').default);
 
-export default [
-  {
-    name: 'Features',
-    target: '#features',
-  },
-];
+export default [];
