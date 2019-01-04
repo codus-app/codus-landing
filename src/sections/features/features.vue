29da438629a452bde1793bbb5e27d28aed8d313a
Luke Taylor <luke@deentaylor.com>
2019-01-04 10:26:40 -0500

    Clip illustrations to the bottom edge of the circle but not the top

--- src/sections/features/features.vue
+++ src/sections/features/features.vue
@@ -13,6 +13,12 @@
         <div class="background"></div>
       </div>
     </div>
+
+    <svg viewBox="0 0 500 500" width="0" height="0">
+      <clipPath id="illustration-mask" clipPathUnits="objectBoundingBox">
+        <path d="M0.5,1 C0.776142375,1 1,0.776142375 1,0.5 C1,0.315905083 1,0.0659050834 1,-0.25 L0,-0.25 C0,0.0659050834 0,0.315905083 0,0.5 0,0.776142375 0.223857625,1 0.5,1 Z"></path>
+      </clipPath>
+    </svg>
   </section>
 </template>
 
