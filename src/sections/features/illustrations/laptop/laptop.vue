dfc54ec73766a2f26e5a61d3221bcd472e2e8664
Luke Taylor <luke@deentaylor.com>
2019-01-04 11:00:25 -0500

    Begin an adaptation of my personal website's 3d CSS 'laptops' to a Vue component (for use in illustrations)

---
+++ src/sections/features/illustrations/laptop/laptop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="laptop3d">
+    <div class="wrapper" ref="wrapper" v-bind:style="wrapperStyle">
+
+      <div class="base" ref="base"><div class="front"></div></div>
+
+      <div class="lid" ref="lid" v-bind:style="lidStyle">
+        <div class="front">
+          <div class="screen" ref="screen">
+            <slot></slot>
+          </div>
+        </div>
+        <div class="back"></div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+<style scoped lang="sass" src="./style.sass"></style>
---
+++ src/sections/features/illustrations/laptop/script.js
@@ -0,0 +1,42 @@
+const sassLengthVariable = 30; // vw
+const sassWidthVariable = (sassLengthVariable / 3) * 2;
+const sassHeightVariable = (sassLengthVariable / 50); // eslint-disable-line no-unused-vars
+
+export default {
+  props: {
+    transitionTime: { type: Number, default: 0.5 },
+
+    rotateX: { type: String, default: '0' },
+    rotateY: { type: String, default: '0' },
+    rotateZ: { type: String, default: '0' },
+    translateX: { type: String, default: '0' },
+    translateY: { type: String, default: '0' },
+    translateZ: { type: String, default: '0' },
+
+    lidAngle: { type: String, default: '100deg' },
+  },
+
+  computed: {
+    wrapperStyle() {
+      return {
+        transform: [
+          `rotateX(${this.rotateX})`,
+          `rotateY(${this.rotateY})`,
+          `rotateZ(${this.rotateZ})`,
+          `translateX(${this.translateX})`,
+          `translateY(${this.translateY})`,
+          `translateZ(${this.translateZ})`,
+        ].join(' '),
+        transition: `transform ${this.transitionTime}s`,
+      };
+    },
+
+    lidStyle() {
+      return {
+        transform: `translateY(-${sassWidthVariable * 2}vw) rotateX(${-180 + this.lidAngle}deg)`,
+        transition: `transform ${this.transitionTime}s`,
+      };
+    },
+
+  },
+};
