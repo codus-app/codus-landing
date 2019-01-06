49c961759a8d6c3004b5b17786e5b909045963f1
Luke Taylor <luke@deentaylor.com>
2019-01-06 01:35:47 -0500

    Down & up methods for features section laptop

--- src/sections/features/script.js
+++ src/sections/features/script.js
@@ -15,8 +15,28 @@ export default {
       const { laptopTransition } = this;
       const [transitionTime, transitionEasing = 'ease'] = laptopTransition;
       return { ...this.laptopPose, transitionTime, transitionEasing };
-      return { ...this.laptopPose };
+    },
   },
+
+  methods: {
+    down() {
+      this.laptopTransition = [0.5, 'ease-in'];
+      this.laptopOpacity = 0;
+      this.laptopPose = {
+        rotateZ: 135,
+        lidAngle: 20,
+        translateZ: -350,
+      };
+    },
+
+    async up() {
+      this.laptopTransition = [0];
+      this.laptopOpacity = 1;
+      this.laptopPose = { rotateZ: -270, lidAngle: 0, translateZ: -500 };
+      await this.$nextTick();
+      this.laptopTransition = [1];
+      this.laptopPose = {};
+    },
   },
 
   components: {
