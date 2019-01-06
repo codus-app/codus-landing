1d9c1f3a4c725a2d257966e2eab25ef64794094a
Luke Taylor <luke@deentaylor.com>
2019-01-06 12:02:49 -0500

    Bind laptops to selected features

--- src/sections/features/script.js
+++ src/sections/features/script.js
@@ -6,6 +6,13 @@ export default {
     selectedIndex: 0,
   }),
 
+  watch: {
+    async selectedIndex(selected, oldSelected) {
+      await this.$refs.laptops[oldSelected].out();
+      if (this.selectedIndex === selected) this.$refs.laptops[selected].in();
+    },
+  },
+
   components: {
     laptop: require('./illustrations/laptop.js').default,
   },
