46a4486520a22c167aa3251e4e8c9828e15bb6fc
Luke Taylor <luke@deentaylor.com>
2019-01-10 22:48:13 -0500

    Scrolling code animation on 'auto-save' illustraiton

--- src/sections/features/illustrations/autosave/script.js
+++ src/sections/features/illustrations/autosave/script.js
@@ -1,5 +1,36 @@
+const colors = [
+  '#8fbcbb',
+  '#88c0d0',
+  '#81a1c1',
+  '#5e81ac',
+  '#4c566a',
+  '#2e3440',
+];
 const fps = 60;
 const delay = 1000 / fps;
+
+
+/* eslint-disable lines-between-class-members */
+class CodeBlock {
+  static get height() { return 6 * window.devicePixelRatio; }
+  static get speed() { return 100 * window.devicePixelRatio; }
+
+  constructor(x, width, canvas) {
+    this.canvas = canvas;
+    this.x = x; this.width = width;
+    this.startTime = new Date();
+    this.color = colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  get delta() { return (new Date() - this.startTime) / 1000; }
+  get y() { return this.canvas.height - this.delta * CodeBlock.speed; }
+  get offScreen() { return this.y < -CodeBlock.height; }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, CodeBlock.height);
+  }
+}
 
 
 export default {
@@ -14,6 +45,10 @@ export default {
 
   mounted() {
     this.ctx = this.$refs.canvas.getContext('2d');
+    this.rows = [];
+    this.addRow();
+    this.$refs.canvas.width = this.$refs.canvas.offsetWidth * window.devicePixelRatio;
+    this.$refs.canvas.height = this.$refs.canvas.offsetHeight * window.devicePixelRatio;
 
     this.start();
   },
@@ -25,7 +60,29 @@ export default {
 
     start() { this.interval = setInterval(() => requestAnimationFrame(() => this.draw()), delay); },
     stop() { clearInterval(this.interval); },
+
+    addRow() {
+      const row = [];
+      const dpr = window.devicePixelRatio;
+      const horizGap = 4 * dpr;
+      const indent = (15 + Math.round(Math.random()) * 20) * dpr;
+      for (let i = 0; i < Math.floor(Math.random() * 5 + 1); i += 1) {
+        const newX = indent + row.map(block => block.width + horizGap).reduce((a, b) => a + b, 0);
+        const width = Math.floor(Math.random() * 50 + 5) * dpr;
+        row.push(new CodeBlock(newX, width, this.$refs.canvas));
+      }
+      this.rows.push(row);
+    },
+
     draw() {
+      const { canvas } = this.$refs;
+      this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+      const bottomRow = this.rows.slice(-1)[0];
+
+      this.rows = this.rows.filter(r => !r[0].offScreen);
+      const gap = 5 * window.devicePixelRatio;
+      if (!bottomRow || bottomRow[0].y < canvas.height - (CodeBlock.height + gap)) this.addRow();
+      [].concat(...this.rows).forEach(b => b.draw(this.ctx));
     },
   },
 
