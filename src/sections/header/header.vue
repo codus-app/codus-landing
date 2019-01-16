a807aa6d36af4720585bcce23af7a62b5e2d8129
Luke Taylor <luke@deentaylor.com>
2019-01-16 10:54:58 -0500

    Header text animation in header

--- src/sections/header/header.vue
+++ src/sections/header/header.vue
@@ -3,18 +3,20 @@
 
     <div class="container">
       <div class="left">
-        <h1>
-          Write, Run,<br><span class="green">Solve.</span>
+        <h1 v-header-text-entrance="{ delay: '.35s' }">
+          <span class="anim">Write,</span> <span class="anim">Run,</span>
+          <br>
+          <span class="anim"><span class="green">Solve.</span></span>
         </h1>
 
-        <p v-fade.up="{ delay: '0.5s' }" data-content="
+        <p v-fade.up="{ delay: '1.1s' }" data-content="
           Learn to solve practical problems with software. Master principles of programming whether
           you’re an amateur, a student, or a professional.
         "></p>
       </div>
 
       <div class="right">
-        <entry-form v-fade.up="{ delay: '0.75s', duration: '.85s' }"></entry-form>
+        <entry-form v-fade="{ delay: '1.6s', duration: '1s' }"></entry-form>
       </div>
     </div>
 
