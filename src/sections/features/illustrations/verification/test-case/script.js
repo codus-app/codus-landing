ce616d6c4ec0f97dfdb9466c2b49a232c78100c7
Luke Taylor <luke@deentaylor.com>
2019-01-18 22:58:51 -0500

    Change position of test-cases

--- src/sections/features/illustrations/verification/test-case/script.js
+++ src/sections/features/illustrations/verification/test-case/script.js
@@ -1,5 +1,39 @@
+const containerWidth = 286;
+
 export default {
   props: {
     success: { type: Boolean, default: null }, // true is sucess, false is failure, null is neutral
+    position: { type: Number, default: 0 },
   },
+
+  data: function data() {
+    return {
+      style: {
+        transform: this.getTransform(),
+        transitionDuration: '0.35s',
+      },
+    };
+  },
+
+  methods: {
+    getTransform() { return `translateX(${(containerWidth / 4) * this.position}px)`; },
+  },
+
+  watch: {
+    position(newPos, oldPos) {
+      this.style.transform = this.getTransform();
+
+      // If we're switching from one side to the other, don't transition
+      if (Math.sign(newPos) && Math.sign(oldPos) && Math.sign(newPos) !== Math.sign(oldPos)) {
+        this.style.transitionDuration = '0s';
+
+        // Set transition duration back
+        this.$nextTick()
+          .then(() => setTimeout(() => {
+            this.style.transitionDuration = '0.35s';
+          }, 40));
+      }
+    },
+  },
+
 };
--- src/sections/features/illustrations/verification/test-case/test-case.vue
+++ src/sections/features/illustrations/verification/test-case/test-case.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="test-case" v-bind:class="{ success: success === true, failure: success === false }">
+  <div
+    class="test-case"
+    v-bind:class="{ success: success === true, failure: success === false }"
+    v-bind:style="style"
+  >
     <icon-check-circle></icon-check-circle>
     <icon-x-circle></icon-x-circle>
   </div>
