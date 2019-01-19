faec84b92d13e4203f3a0e0a8e3b6efd6977ee4d
Luke Taylor <luke@deentaylor.com>
2019-01-18 21:41:43 -0500

    Add first test case

--- src/sections/features/illustrations/verification/verification.vue
+++ src/sections/features/illustrations/verification/verification.vue
@@ -2,6 +2,9 @@
   <div class="illustration">
     <laptop ref="laptop" v-bind="{ initialVisibleState }">
       <icon-file></icon-file>
+      <div class="test-cases">
+        <div class="test"></div>
+      </div>
     </laptop>
   </div>
 </template>
