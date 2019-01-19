399e3057e417a1de88ec4ebfd2fef88aa376996d
Luke Taylor <luke@deentaylor.com>
2019-01-19 11:24:14 -0500

    Attempt to communicate saving in auto-save illustration

--- src/sections/features/illustrations/autosave/autosave.vue
+++ src/sections/features/illustrations/autosave/autosave.vue
@@ -2,6 +2,12 @@
   <div class="illustration">
     <laptop ref="laptop" v-bind="{ initialVisibleState, typing: true }">
       <canvas ref="canvas"></canvas>
+      <div class="rings" slot="base">
+        <div class="ring" slot="base"
+          v-for="i in 4"
+          v-bind:key="i"
+        ></div>
+      </div>
     </laptop>
   </div>
 </template>
