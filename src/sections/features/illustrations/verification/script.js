290c01507784a69ddb39cf73c273dd8e09628cca
Luke Taylor <luke@deentaylor.com>
2019-01-18 23:48:14 -0500

    functional animation of verification illustration

--- src/sections/features/illustrations/verification/script.js
+++ src/sections/features/illustrations/verification/script.js
@@ -5,11 +5,8 @@ export default {
 
   data: () => ({
     testCaseProps: new Array(7).fill({}),
+    timeout: null,
   }),
-
-  mounted() {
-    this.reset();
-  },
 
   computed: {
     // The index (in the testCaseProps array) of the testCase that occupies the center position
@@ -22,8 +19,10 @@ export default {
     outQuick() { return this.$refs.laptop.outQuick(); },
 
     reset() {
+      clearTimeout(this.timeout);
       this.testCaseProps = new Array(7).fill(null)
         .map((_, i) => ({ success: null, position: -1 - i }));
+      this.timeout = setTimeout(() => this.step(), 1300);
     },
 
     // Slide test cases to the side, and reset test cases from the far left to the far right
@@ -33,6 +32,16 @@ export default {
         ? { ...n, position: n.position + 1 }
         // If we've gotten off screen, reset to the right
         : { success: null, position: -3 }));
+    },
+
+    step() {
+      this.move();
+      this.timeout = setTimeout(() => {
+        this.testCaseProps[this.centerIndex].success = Math.random() <= 0.66;
+        this.timeout = setTimeout(() => {
+          this.step();
+        }, 500);
+      }, 650);
     },
   },
 
