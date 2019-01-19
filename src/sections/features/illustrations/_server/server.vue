38f9150b269813237ff13b77af9c35e31588bc5a
Luke Taylor <luke@deentaylor.com>
2019-01-19 17:28:11 -0500

    Add 3d server illustration

---
+++ src/sections/features/illustrations/_server/server.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="server" v-bind:style="style">
+    <div class="server3d">
+      <div class="wrapper" ref="wrapper" v-bind:style="wrapperStyle">
+        <div class="box">
+          <div class="left"></div>
+          <div class="right"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style scoped lang="sass" src="./style.sass"></style>
