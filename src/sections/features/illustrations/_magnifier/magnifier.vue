c0a8a13e3605287b72e5d0f382af9f6d02ac4daa
Luke Deen Taylor <luke@deentaylor.com>
2019-01-21 00:53:52 -0500

    Add 3D CSS 'magnifier' component

---
+++ src/sections/features/illustrations/_magnifier/magnifier.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="magnifier" v-bind:style="style">
+    <div class="magnifier3d">
+      <div class="wrapper" v-bind:style="wrapperStyle">
+        <div class="handle"></div>
+        <div class="lens">
+          <div class="contents">
+
+            <slot></slot>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+<style scoped lang="sass" src="./style.sass"></style>
---
+++ src/sections/features/illustrations/_magnifier/script.js
@@ -0,0 +1,82 @@
+function suffix(val, suf) {
+  if (typeof val === 'string' && val.match(/[^0-9.]/)) return val; // If it's not just numbers, assume it already has a suffix
+  return `${val}${suf}`; // Otherwise add the given suffix
+}
+function px(val) { return suffix(val, 'px'); } // Give values a default unit of 'px'
+function deg(val) { return suffix(val, 'deg'); } // Give values a default unit of 'deg'
+function secs(val) { return suffix(val, 's'); } // Give values a default unit of 's'
+
+export default {
+  props: { initialVisibleState: Boolean },
+
+  data: () => ({
+    transitionTime: 0.5,
+    transitionEasing: 'ease',
+    opacity: 1,
+
+    translateX: 0,
+    translateZ: 0,
+    rotateZ: 0,
+  }),
+
+  created() {
+    if (!this.initialVisibleState) this.out();
+  },
+
+  computed: {
+    wrapperStyle() {
+      return {
+        transform: [
+          `translateX(${px(this.translateX)})`,
+          `translateZ(${px(this.translateZ)})`,
+          `rotateZ(${deg(this.rotateZ)})`,
+        ].join(' '),
+        transition: `transform ${secs(this.transitionTime)} ${this.transitionEasing}`,
+      };
+    },
+    style() { return { opacity: this.opacity, transition: `opacity ${secs(this.transitionTime)} ${this.transitionEasing}` }; },
+  },
+
+  methods: {
+    async out(duration = 0.35) {
+      this.$emit('animationstart');
+      this.$emit('animationend');
+      Object.assign(this, {
+        transitionTime: duration,
+        transitionEasing: 'ease-in',
+        opacity: 0,
+        rotateZ: 135,
+        translateZ: -350,
+      });
+
+      await new Promise(resolve => setTimeout(resolve, duration * 1000));
+      this.$emit('animationend');
+    },
+
+    async in(duration = 1) {
+      this.$emit('animationstart');
+
+      // Set initial position
+      Object.assign(this, {
+        transitionTime: 0,
+        opacity: 1,
+        rotateZ: -50,
+        translateX: 600,
+        translateZ: -100,
+      });
+      // Wait for CSS to be applied
+      await this.$nextTick();
+      await new Promise(resolve => setTimeout(resolve, 20)); // Safari & firefox need extra time
+      // Transition up
+      Object.assign(this, {
+        transitionTime: duration,
+        transitionEasing: 'ease',
+        rotateZ: 0,
+        translateX: 0,
+        translateZ: 0,
+      });
+      await new Promise(resolve => setTimeout(resolve, duration * 1000));
+      this.$emit('animationend');
+    },
+  },
+};
