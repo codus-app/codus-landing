10e8da20db7f5f5f964227a09172c42717e2ac08
Luke Taylor <luke@deentaylor.com>
2019-03-06 09:37:45 -0500

    Fix supported browser check to reject Edge

--- src/browser-detect.js
+++ src/browser-detect.js
@@ -1,7 +1,8 @@
 function bool(val) { return !!val; }
 
-// Chrome or most other Chrome-like browsers (like Opera and Brave)
-export const isChrome = bool(window.chrome);
+// Chrome or most other Chrome-like browsers (like Opera and Brave). Edge defines window.chrome
+// but is missing loadTimes
+export const isChrome = bool(window.chrome && window.chrome.loadTimes);
 // Firefox
 export const isFirefox = bool(window.InstallTrigger);
 
