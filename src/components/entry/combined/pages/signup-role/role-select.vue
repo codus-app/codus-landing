6f0aa8c21278dc1c7a6d2a59f7c15e84ab30e3ea
Luke Taylor <luke@deentaylor.com>
2019-04-08 10:38:55 -0400

    Begin role select page of signup

---
+++ src/components/entry/combined/pages/signup-role/role-select.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page role-select">
+    <h3 class="green" slot="heading">Welcome aboard!</h3>
+
+    <h4>Which best describes you?</h4>
+    <div class="role-select">
+      <div
+        class="role"
+        v-bind:class="{ selected: value === 'student' }"
+        v-on:click="$emit('input', 'student')"
+      >
+        <h5>Student</h5>
+        <p>Learn, practice, and improve your programming ability</p>
+      </div>
+      <div
+        class="role"
+        v-bind:class="{ selected: value === 'instructor' }"
+        v-on:click="$emit('input', 'instructor')"
+      >
+        <h5>Instructor</h5>
+        <p>Create classrooms, manage students, and assign homework</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style scoped lang="sass" src="./style.sass"></style>
